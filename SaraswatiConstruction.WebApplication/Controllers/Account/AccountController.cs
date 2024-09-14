9b9d90ddf7a05cfd4088457fcf0f6b8bcd094029
Raj Bhatt <rajbhattlearning2023@gmail.com>
2024-09-14 12:09:38 +0530

    Add project files.

---
+++ SaraswatiConstruction.WebApplication/Controllers/Account/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaraswatiConstruction.WebApplication.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SaraswatiConstruction.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SaraswatiConstruction.WebApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
