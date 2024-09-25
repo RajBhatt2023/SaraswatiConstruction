f06a99eb24eb75a35b5772f3b2a32b55df395823
Vikas Chauhan <vikaschauhan273014@gmail.com>
2024-09-26 01:22:43 +0530

    #Added Data Layer

---
+++ SaraswatiConstruction.Infrastructure/DataAccess/DataAccessHelper.cs
@@ -0,0 +1,83 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using TaxNetworkUSA_FINT.Infrastructure.DataAccess.Interface;
+
+namespace TaxNetworkUSA_FINT.Infrastructure.DataAccess
+{
+    public class DataAccessHelper : IDataAccessHelper
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string? _connectionString;
+
+        public DataAccessHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SqlConnection");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+
+            return new SqlConnection(_connectionString);
+        }
+
+        public async Task<IEnumerable<T>> ExecuteStoredProcedureDataTableAsync<T>(string procedureName)
+        {
+            using (var connection = CreateConnection())
+            {
+
+                var result = await connection.QueryAsync<T>(procedureName, commandType: CommandType.StoredProcedure);
+
+                return result;
+            }
+        }
+
+        public async Task<IEnumerable<T>> ExecuteStoredProcedureDataTableAsync<T>(string procedureName, object parameters)
+        {
+            using (var connection = CreateConnection())
+            {
+                return await connection.QueryAsync<T>(procedureName, parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public async Task<List<IEnumerable<T>>> ExecuteStoredProcedureDataSetAsync<T>(string procedureName)
+        {
+            using (var connection = CreateConnection())
+            {
+                using (var multi = await connection.QueryMultipleAsync(procedureName, commandType: CommandType.StoredProcedure))
+                {
+                    var resultSets = new List<IEnumerable<T>>();
+
+                    while (!multi.IsConsumed)
+                    {
+                        var resultSet = await multi.ReadAsync<T>();
+                        resultSets.Add(resultSet);
+                    }
+                    return resultSets;
+                }
+            }
+        }
+
+        public async Task<List<IEnumerable<T>>> ExecuteStoredProcedureDataSetAsync<T>(string procedureName, object parameters)
+        {
+            using (var connection = CreateConnection())
+            {
+                using (var multi = await connection.QueryMultipleAsync(procedureName, parameters, commandType: CommandType.StoredProcedure))
+                {
+                    var resultSets = new List<IEnumerable<T>>();
+
+                    while (!multi.IsConsumed)
+                    {
+                        var resultSet = await multi.ReadAsync<T>();
+                        resultSets.Add(resultSet);
+                    }
+
+                    return resultSets;
+                }
+            }
+        }
+
+    }
+}
---
+++ SaraswatiConstruction.Infrastructure/DataAccess/Interface/IDataAccessHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxNetworkUSA_FINT.Infrastructure.DataAccess.Interface
+{
+    public interface IDataAccessHelper
+    {
+        public Task<IEnumerable<T>> ExecuteStoredProcedureDataTableAsync<T>(string procedureName);
+        public Task<IEnumerable<T>> ExecuteStoredProcedureDataTableAsync<T>(string procedureName, object parameters);
+        public Task<List<IEnumerable<T>>> ExecuteStoredProcedureDataSetAsync<T>(string procedureName);
+        public Task<List<IEnumerable<T>>> ExecuteStoredProcedureDataSetAsync<T>(string procedureName, object parameters);
+    }
+}
